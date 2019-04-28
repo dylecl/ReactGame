b5486b34426dc7a3792bc0cdf81c9b80365f7655
dylecl <dylann.leclairr@gmail.com>
2019-04-28 17:48:57 -0600

    Updates

---
+++ precache-manifest.4e6e3413ebdbf61a97655dfed448138c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e4028a9c2dc11f6098b0174513d291",
+    "url": "/ReactGame/index.html"
+  },
+  {
+    "revision": "0c0ec0437f05a7f0aeb5",
+    "url": "/ReactGame/static/css/main.be7e095b.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/ReactGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "0c0ec0437f05a7f0aeb5",
+    "url": "/ReactGame/static/js/main.0cc3f4c5.chunk.js"
+  },
+  {
+    "revision": "77e357886314402729a3",
+    "url": "/ReactGame/static/js/runtime~main.d5ea2d59.js"
+  }
+]);
